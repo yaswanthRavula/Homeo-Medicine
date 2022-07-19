0645e02a11580d2561a06f822d752063c5f542ba
Yaswanth <yaswanthravula0@gmail.com>
2022-07-19 21:35:48 +0530

    vbjmn,

--- client/index.js
+++ client/index.js
@@ -1,12 +1,10 @@
 const express= require('express');
 const app=express();
-app.use(express.static("./dist/client/"))
 const cors=require('cors');
 const bodyParser=require('body-parser');
 const mongoose=require('mongoose');
 app.use(bodyParser.json());
 app.use(cors());
-
 app.listen((process.env.PORT || 3000),()=>{console.log("Backend Server Listening at 3000 Port")});
 mongoose.connect("mongodb+srv://yaswanth07:Yy8309328761@cluster0.fgrp3.mongodb.net/?retryWrites=true&w=majority")
 .then(console.log("MongoDb Connected"));
