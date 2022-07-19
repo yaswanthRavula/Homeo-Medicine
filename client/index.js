37fc8df4351cdd326c75c8f5a49a1afd188308b3
Yaswanth <yaswanthravula0@gmail.com>
2022-07-19 16:31:38 +0530

    asadsx

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 const express= require('express');
 const app=express();
+app.use(express.static(__dirname+"/dist/"))
 const cors=require('cors');
 const bodyParser=require('body-parser');
 const mongoose=require('mongoose');
