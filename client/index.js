2f9d15f67035bdfe6f287c1e5613d6d75cc9953a
Yaswanth <yaswanthravula0@gmail.com>
2022-07-21 15:41:31 +0530

    Final Submit

--- client/index.js
+++ client/index.js
@@ -9,6 +9,14 @@ app.use(bodyParser.json());
 app.get('/*', (req,res)=>{
     res.sendFile(path.join(__dirname,'dist/client','index.html'))
 })
+
+
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 app.listen((process.env.PORT || 3000),()=>{console.log("Backend Server Listening at 3000 Port")});
 mongoose.connect("mongodb+srv://yaswanth07:Yy8309328761@cluster0.fgrp3.mongodb.net/?retryWrites=true&w=majority")
 .then(console.log("MongoDb Connected"));
