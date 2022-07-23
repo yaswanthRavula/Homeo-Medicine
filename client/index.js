c5537457a5e36c77b8cf622e9171a2bb0824377f
Yaswanth <yaswanthravula0@gmail.com>
2022-07-23 23:01:06 +0530

    asa

--- client/index.js
+++ client/index.js
@@ -9,21 +9,6 @@ app.use(bodyParser.json());
 app.get('/*', (req,res)=>{
     res.sendFile(path.join(__dirname,'dist/client','index.html'))
 })
-
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
-
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
 app.listen((process.env.PORT || 3000),()=>{console.log("Backend Server Listening at 3000 Port")});
 mongoose.connect("mongodb+srv://yaswanth07:Yy8309328761@cluster0.fgrp3.mongodb.net/?retryWrites=true&w=majority")
 .then(console.log("MongoDb Connected"));
