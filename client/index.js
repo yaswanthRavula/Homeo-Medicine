546ba1991dbb902a40e06fd4fa83efdece86960d
Yaswanth <yaswanthravula0@gmail.com>
2022-07-23 23:12:46 +0530

    sas

--- client/index.js
+++ client/index.js
@@ -9,8 +9,16 @@ app.use(bodyParser.json());
 app.get('/*', (req,res)=>{
     res.sendFile(path.join(__dirname,'dist/client','index.html'))
 })
+
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");    
+    next();
+  });
+
 app.listen((process.env.PORT || 3000),()=>{console.log("Backend Server Listening at 3000 Port")});
 mongoose.connect("mongodb+srv://yaswanth07:Yy8309328761@cluster0.fgrp3.mongodb.net/?retryWrites=true&w=majority")
 .then(console.log("MongoDb Connected"));
 const PatientRouter=require('./controller/PatientController');
-app.use('/HomeoMedicine/patients/',PatientRouter);
+app.use('/HomeoMedicine/patients',PatientRouter);
