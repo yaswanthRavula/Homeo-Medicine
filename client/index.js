cf0beea3e1dd61e990d8004a96ad81a5c6beee0b
Yaswanth <yaswanthravula0@gmail.com>
2022-07-24 16:03:44 +0530

    qwasa

--- client/index.js
+++ client/index.js
@@ -1,14 +1,11 @@
 const express= require('express');
 const path=require("path");
 const app=express();
-app.use(express.static(__dirname+'/dist/client/'))
 const cors=require('cors');
 const bodyParser=require('body-parser');
 const mongoose=require('mongoose');
 app.use(bodyParser.json());
-app.get('/*', (req,res)=>{
-    res.sendFile(path.join(__dirname,'dist/client','index.html'))
-})
+
 
 
 app.use(function(req, res, next) {
