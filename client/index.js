0117d4f5dd299017077a3e6b8ac7a7c50df47018
Yaswanth <yaswanthravula0@gmail.com>
2022-07-24 16:24:11 +0530

    asscdsa

--- client/index.js
+++ client/index.js
@@ -1,5 +1,5 @@
 const express= require('express');
-const path=require("path");
+const path=require('path');
 const app=express();
 app.use(express.static(__dirname+'/dist/client/'))
 const cors=require('cors');
