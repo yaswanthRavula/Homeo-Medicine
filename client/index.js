479a7c9854c90e5edc429b0cfbba81307cbcb542
Yaswanth <yaswanthravula0@gmail.com>
2022-07-24 16:05:32 +0530

    aszx

--- client/index.js
+++ client/index.js
@@ -1,19 +1,29 @@
 const express= require('express');
 const path=require("path");
 const app=express();
+app.use(express.static(__dirname+'/dist/client/'))
 const cors=require('cors');
 const bodyParser=require('body-parser');
 const mongoose=require('mongoose');
 app.use(bodyParser.json());
-
-
+app.get('/*', (req,res)=>{
+    res.sendFile(path.join(__dirname,'dist/client','index.html'))
+})
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");    
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
-  });
+});
 
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 app.listen((process.env.PORT || 3000),()=>{console.log("Backend Server Listening at 3000 Port")});
 mongoose.connect("mongodb+srv://yaswanth07:Yy8309328761@cluster0.fgrp3.mongodb.net/?retryWrites=true&w=majority")
 .then(console.log("MongoDb Connected"));
