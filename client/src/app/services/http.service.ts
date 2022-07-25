220fad04a1b1a4dc741db724fe7f38b55987ee83
Yaswanth <yaswanthravula0@gmail.com>
2022-07-25 20:33:41 +0530

    sas

--- client/src/app/services/http.service.ts
+++ client/src/app/services/http.service.ts
@@ -7,7 +7,7 @@ import { PatientDetails } from './patient-details';
   providedIn: 'root'
 })
 export class HttpService {
-APiUrl="https://murali-homeo.herokuapp.com/3000/HomeoMedicine/patients";
+APiUrl="https://murali-homeo.herokuapp.com/asasasHomeoMedicine/patients";
   constructor(private http:HttpClient ) { }
 
   postPatient(patient: PatientDetails){
